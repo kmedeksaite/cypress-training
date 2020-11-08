cb3e69b5822335cee7886c3323c4984e020a6122
Lucas A Castro <lucas.castro@zohomail.com>
2020-11-07 21:28:39 -0700

    implement add and delete features

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { FiPlus } from 'react-icons/fi';
 
 import TodoInput from './components/TodoItem';
@@ -5,16 +6,57 @@ import TodoInput from './components/TodoItem';
 import './App.css';
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem('@todo-testing:todos');
+
+    if (storedTodos) return JSON.parse(storedTodos);
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@todo-testing:todos', JSON.stringify(todos));
+  }, [todos]);
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+
+    if (!newItem) {
+      alert('Please, enter a todo item.');
+      return;
+    }
+
+    setTodos([...todos, newItem]);
+    setNewItem('');
+  }
+
+  function deleteItem(itemId) {
+    const allTodos = [...todos].filter((_, index) => index !== itemId);
+
+    setTodos([...allTodos]);    
+  }
+
   return (
     <div className="app">
       <h1>To-do Testing</h1>
 
-      <form>
-        <input type="text" name="add-todo" id="add-todo" placeholder="add new todo..." />
+      <form onSubmit={handleAddTodo}>
+        <input 
+          type="text"
+          name="add-todo"
+          id="add-todo"
+          placeholder="add new todo..."
+          value={newItem}
+          onChange={e => setNewItem(e.target.value)}
+        />
         <button type="submit"><FiPlus /></button>
       </form>
 
-      <TodoInput />
+
+      {todos.map((item, index) => (
+        <TodoInput itemDescription={item} itemId={index} onDelete={deleteItem} />
+      ))}
     </div>
   )
 }
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { FiX } from 'react-icons/fi';
 
 import './style.css';
 
-function TodoItem() {
+function TodoItem({ onDelete, itemId, itemDescription}) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleBoxClick() {
+    const toggledValue = !isChecked;
+
+    setIsChecked(toggledValue);
+  }
+
+  function handleDelete() {
+    onDelete(itemId)
+  }
+
   return (
     <div className="todo-item">
-      <input type="checkbox" name="item" id="item"/>
-      <label htmlFor="item">Item</label>
-      <button><FiX /></button>
+      <input type="checkbox" name={itemId} id={itemId} checked={isChecked} onClick={handleBoxClick} />
+      <label htmlFor={itemId} className={isChecked ? 'striked' : ''}>{itemDescription}</label>
+      <button onClick={handleDelete}><FiX /></button>
     </div>
   )
 }
